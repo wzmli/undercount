e621ba664a5129f425de6b54f6529e7320fa1c93
Ben Bolker <bbolker@gmail.com>
2022-09-21 21:24:13 -0400

    improve install_pkgs

--- install_pkgs.R
+++ install_pkgs.R
@@ -1,10 +1,11 @@
 cran_pkgs <- c("cowplot", "directlabels", "ggplot2", "tikzDevice","remotes", "asymptor")
 
-github_pkgs <- c("dushoff/shellpipes", "mac-theobio/McMasterPandemic")
+github_pkgs <- c("dushoff/shellpipes", "mac-theobio/McMasterPandemic", "clauswilke/colorblindr")
 
 i1 <- installed.packages()
 cran_pkgs <- setdiff(cran_pkgs, rownames(i1))
 
 if (length(cran_pkgs)>0) install.packages(cran_pkgs)
-sapply(github_pkgs, remotes::install_github)
+## MacPan seems to be problematic -- too big?
+sapply(github_pkgs, function(x) try(remotes::install_github(x)))
 
