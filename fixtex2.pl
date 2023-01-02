6daac5f0f9acc5c8412835dd421632b7ab3ae645
wzmli <wzmichael.li@gmail.com>
2023-01-02 08:04:10 -0500

    Need to add correspondence, need to hack JD's pearl script?

---
+++ fixtex2.pl
@@ -0,0 +1,15 @@
+
+use strict;
+use 5.10.0;
+
+$/ = undef;
+
+open INC, "<", grep(/inc$/, @ARGV) or die;
+my $inc = <INC>;
+
+open TEX, "<", grep(/tex$/, @ARGV) or die;
+my $tex = <TEX>;
+
+$tex =~ s/\\author[^}]*}/$inc/;
+
+print $tex;
